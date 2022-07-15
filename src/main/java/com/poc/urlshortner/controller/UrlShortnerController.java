21e799ec9fc4b1a8afbef29eab2897aec4043bef
Balu Malasani <balu.malasani@gmail.com>
2022-07-15 23:27:32 +0530

    Use case 1: Redirecting user to default page and show form to generate shorten urls.
    Redirect user to results page with hardcoded values.

--- src/main/java/com/poc/urlshortner/controller/UrlShortnerController.java
+++ src/main/java/com/poc/urlshortner/controller/UrlShortnerController.java
@@ -1,7 +1,37 @@
 package com.poc.urlshortner.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.poc.urlshortner.model.UrlShortenModel;
+
+@Controller
 public class UrlShortnerController {
-
-	
+    /*
+     * To handle default page rendering
+     */
+	@GetMapping("/")
+	public String homePage(Model model) {
+		model.addAttribute("shortenmodel", new UrlShortenModel());
+		return "index";
+	}
+	/*
+	 * To handle shorten urls 
+	 */
+	@GetMapping("/{url}")
+	public String redirectToActualUrl() {
+		return null;
+	}
+	/*
+	 * To generate shorten url and return to results page.
+	 */
+	@PostMapping("/generateUrl")
+	public String saveShortenUrl(@ModelAttribute UrlShortenModel shortenmodel,Model model ) {
+		shortenmodel.setShortenUrl("http://localhost:8080/123"); // Need to get from DB 
+		model.addAttribute("shortenmodel", shortenmodel);
+		return "results";
+	}
 }
--- src/main/java/com/poc/urlshortner/model/UrlShortenModel.java
+++ src/main/java/com/poc/urlshortner/model/UrlShortenModel.java
@@ -1,5 +1,30 @@
 package com.poc.urlshortner.model;
 
 public class UrlShortenModel {
+	
+	private String url;
+	
+	private String shortenUrl;
+	
 
+	public String getShortenUrl() {
+		return shortenUrl;
+	}
+
+
+	public void setShortenUrl(String shortenUrl) {
+		this.shortenUrl = shortenUrl;
+	}
+
+
+	public String getUrl() {
+		return url;
+	}
+
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	
 }
